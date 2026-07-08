1ff27b33c78b096cb6e8643000c4092052e053f0
Catarina F <13944883+Gotharchitect@users.noreply.github.com>
2026-07-08 17:10:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -41,4 +41,4 @@ export const FIREBASE_CONFIG = {
 };
 
 // 4. Set to false once you've filled in FIREBASE_CONFIG above.
-export const IS_PLACEHOLDER = FIREBASE_CONFIG.apiKey === false;
+export const IS_PLACEHOLDER = false;
